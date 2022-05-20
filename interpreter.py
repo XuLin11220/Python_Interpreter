23dbfe3ca0702355b7c0c5b0dc0afba9bba4cf42
XuLin11220 <xulin0770@gmail.com>
2022-05-20 14:21:55 -0400

    Commit

--- interpreter.py
+++ interpreter.py
@@ -9,11 +9,13 @@ class Interpreter:
 
     def program(self):
         while True:
+            # read and store it into curr_token
             self.curr_token = self.file_text.next_token()
             if self.curr_token['type'] == 'EOF':
                 break
             if self.curr_token['type'] == 'Identifier':
                 variable = self.curr_token['token']
+                # read and store it into curr_token
                 self.curr_token = self.file_text.next_token()
                 self.match_token('=')
                 expression = self.expression()
@@ -49,6 +51,7 @@ class Interpreter:
 
     # eliminate left recursion
     def term_prime(self):
+        # read and store it into curr_token
         self.curr_token = self.file_text.next_token()
         if self.curr_token['type'] == '*':
             factor = self.factor()
@@ -57,15 +60,9 @@ class Interpreter:
             return 1
 
     def factor(self):
+        # read and store it into curr_token
         self.curr_token = self.file_text.next_token()
-        if self.curr_token['type'] == 'Literal':
-            return int(self.curr_token['token'])
-        elif self.curr_token['type'] == 'Identifier':
-            if self.curr_token['token'] in self.symbol:
-                return self.symbol.get(self.curr_token['token'])
-            else:
-                raise Exception('Error, worng token')
-        elif self.curr_token['type'] == '+':
+        if self.curr_token['type'] == '+':
             return self.factor()
         elif self.curr_token['type'] == '-':
             return -1 * self.factor()
@@ -73,3 +70,10 @@ class Interpreter:
             result = self.expression()
             self.match_token(')')
             return result
+        elif self.curr_token['type'] == 'Literal':
+            return int(self.curr_token['token'])
+        elif self.curr_token['type'] == 'Identifier':
+            if self.curr_token['token'] in self.symbol:
+                return self.symbol.get(self.curr_token['token'])
+            else:
+                raise Exception('Error, worng token')
